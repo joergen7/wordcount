11aa41ad3867595bbdc115b713b8a6209ef3f929
Jorgen Brandt <joergen.brandt@onlinehome.de>
2016-07-30 21:56:57 +0200

    Cookbook scaffold added.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'wordcount'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures wordcount'
+long_description 'Installs/Configures wordcount'
+version '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: wordcount
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
