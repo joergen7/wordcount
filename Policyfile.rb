b2ae4a02afe2e11bc1743f59258223c91ee6b69d
Jorgen Brandt <joergen.brandt@onlinehome.de>
2017-05-09 12:37:21 +0200

    Bitrot removed.

---
+++ Policyfile.rb
@@ -0,0 +1,18 @@
+# Policyfile.rb - Describe how you want Chef to build your system.
+#
+# For more information on the Policyfile feature, visit
+# https://github.com/opscode/chef-dk/blob/master/POLICYFILE_README.md
+
+# A name that describes what the system you're building with Chef does.
+name "wordcount"
+
+# Where to find external cookbooks:
+default_source :supermarket
+
+# run_list: chef-client will run these recipes in the order specified.
+run_list "wordcount::default"
+
+# Specify a custom source for a single cookbook:
+cookbook "wordcount", path: "."
+cookbook "chef-cuneiform", git: "https://github.com/joergen7/chef-cuneiform.git"
+cookbook "chef-misc", git: "https://github.com/joergen7/chef-misc.git"
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,7 +4,7 @@
 #
 # Copyright (c) 2016 Jörgen Brandt, All Rights Reserved.
 
-include_recipe "chef-cuneiform::default"
-
+include_recipe "wordcount::_common"
+include_recipe "chef-cuneiform::cuneiform"
 include_recipe "wordcount::workflow"
 include_recipe "wordcount::data"
